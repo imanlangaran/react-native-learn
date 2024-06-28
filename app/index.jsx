97d58a64e87fc983f6023ce94573610f8f846efe
Iman Langaran <imanlangaran@gmail.com>
2024-06-28 18:55:25 +0330

    login layout (not completeed)

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,48 @@
-import {  Text, View } from 'react-native'
+import { ScrollView, Text, View, Image } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from "../constants"
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack" >App</Text>
-      <StatusBar style='auto'/>
-      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full h-full items-center px-4 "> 
+          {/* justify-center */}
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image 
+            source={images.cards}
+            className="max-x-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Disscover Endless Possibilities with{' '}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+          </View>
+
+          <Image 
+            source={images.path}
+            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            resizeMode="contain"
+          />
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+          </Text>
+
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
