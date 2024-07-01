3747ac481a32295913ff74fea7843a33be6b5792
Iman Langaran <imanlangaran@gmail.com>
2024-07-01 07:30:22 +0330

    finished logging in

--- app/index.jsx
+++ app/index.jsx
@@ -4,8 +4,13 @@ import { Redirect, router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from "../components/CustomButton";
 import { images } from "../constants"
+import { useGlobalContext } from '../context/GlobalProvider';
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if(!isLoading && isLoggedIn) return <Redirect href={"./home"}/>
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
