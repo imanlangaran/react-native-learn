b402b700cf4fbd64488c6840798dffa30eb8a775
Iman Langaran <imanlangaran@gmail.com>
2024-07-01 07:16:21 +0330

    implement globalProvider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,8 @@ import { Slot, SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react'
 
+import GlobalProvider from '../context/GlobalProvider'
+
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -18,21 +20,23 @@ const RootLayout = () => {
     "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   })
-  
+
   useEffect(() => {
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error])
 
   if (!fontsLoaded && !error) return null;
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
-      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-      {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
+      </Stack>
+    </GlobalProvider>
   )
 }
 
